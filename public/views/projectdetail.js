264592f84002ecbd325ae29dd22033a957f7f8e5
IIPL\13222 <meesam.engineer@gmail.com>
2016-10-04 18:19:14 +0530

    refactoring

--- public/views/projectdetail.js
+++ public/views/projectdetail.js
@@ -6,22 +6,10 @@ issueTrackerApp.registerCtrl('projectdetailcontroller',
     $scope.btntext=$routeParams.ID=="new"?"Save":"Update";
 
     $scope.saveproject=function(){
-        if ( angular.isUndefined($scope.project.ProjectName) || $scope.project.ProjectName == '') {
-              $rootScope.setMsg('Project Name is required');
-              return;
-             }
-        if ( angular.isUndefined($scope.project.Description) || $scope.project.Description == '') {
-              $rootScope.setMsg('Project Description is required');
-              return;
-             }
-
-        console.log($scope.project);
+        if ( angular.isUndefined($scope.project.ProjectName) || $scope.project.ProjectName == '') { $rootScope.setMsg('Project Name is required'); return; }
+        if ( angular.isUndefined($scope.project.Description) || $scope.project.Description == '') { $rootScope.setMsg('Project Description is required'); return; }
         appServices.doActionPost({ Token: $rootScope.token, Obj: $scope.project }, 'projects/add').then(function (d) {
-            if (d.Status == 'success') {
-                 $location.path('/projects');
-                //angular.copy($scope.tempuser, $scope.user); $scope.setEdit(false);
-                //$rootScope.setMsg('User profile ' + ($scope.tempuser.UserID > 0 ? 'updated' : 'created') + ' successfully', true);
-            }
+            if (d.Status == 'success') $location.path('/projects');
             else $rootScope.setMsg(d.Info);
         });
     }
